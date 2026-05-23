4a3bafdf8cdef7440e04677bd56bca06b726dbed
Alama <alama@student.42belgium.be>
2026-05-23 15:06:47 +0200

    [PhysicalDevice & Queue family] : pick the right device or throw error if the required device isn't found

--- src/HelloTriangle/1_pickPhysicalDevice.cpp
+++ src/HelloTriangle/1_pickPhysicalDevice.cpp
@@ -4,14 +4,15 @@
 
 bool HelloTriangleApplication::isDeviceSuitable(
     vk::raii::PhysicalDevice const &physicalDevice) {
-
   bool supportVulkan1_3 =
       physicalDevice.getProperties().apiVersion >= vk::ApiVersion13;
+
   auto queueFamilies = physicalDevice.getQueueFamilyProperties();
   bool supportGraphics =
       std::ranges::any_of(queueFamilies, [](auto const &qfp) {
         return !!(qfp.queueFlags & vk::QueueFlagBits::eGraphics);
       });
+
   auto availableDeviceExtensions =
       physicalDevice.enumerateDeviceExtensionProperties();
   bool supportAllRequiredExtensions = std::ranges::all_of(
@@ -28,7 +29,6 @@ bool HelloTriangleApplication::isDeviceSuitable(
   auto features = physicalDevice.template getFeatures2<
       vk::PhysicalDeviceFeatures2, vk::PhysicalDeviceVulkan13Features,
       vk::PhysicalDeviceExtendedDynamicStateFeaturesEXT>();
-
   bool supportsRequiredFeatures =
       features.template get<vk::PhysicalDeviceVulkan13Features>()
           .dynamicRendering &&
