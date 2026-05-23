44db2ce5c19eaff818c2b8c82bf665febbd1077c
Alama <alama@student.42belgium.be>
2026-05-23 14:28:19 +0200

    [Application] : remove and split some method to better readability

--- include/HelloTriangleApplication.hpp
+++ include/HelloTriangleApplication.hpp
@@ -2,6 +2,7 @@
 
 #define HELLO_TRIANGLE_HPP
 
+#include <vector>
 #if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
 #include <vulkan/vulkan_raii.hpp>
 #else
@@ -12,9 +13,7 @@ import vulkan_hpp;
 #define GLFW_INCLUDE_NONE
 
 #include <GLFW/glfw3.h>
-#include <algorithm>
 #include <vulkan/vulkan.hpp>
-#include <iostream>
 
 constexpr uint32_t WIDTH = 800;
 constexpr uint32_t HEIGHT = 600;
@@ -34,6 +33,9 @@ private:
   GLFWwindow *window = nullptr;
   vk::raii::Context context;
   vk::raii::Instance instance = nullptr;
+  vk::raii::PhysicalDevice physicalDevice = nullptr;
+  std::vector<const char *> requiredDeviceExtension = {
+      vk::KHRSwapchainExtensionName};
 
   void initWindow() {
     glfwInit();
@@ -41,55 +43,15 @@ private:
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
     window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan test", nullptr, nullptr);
   }
- 
-	void initVulkan() { createInstance(); }
 
-  void createInstance() {
-    constexpr vk::ApplicationInfo appInfo{
-        .pApplicationName = "Hello Triangle",
-        .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
-        .pEngineName = "No Engine",
-        .engineVersion = VK_MAKE_VERSION(1, 0, 0),
-        .apiVersion = vk::ApiVersion14};
-
-    uint32_t glfwExtensionCount = 0;
-    const char **glfwExtensions =
-        glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-    if (!glfwExtensions) {
-      throw std::runtime_error("Failed to get GLFW Vulkan extensions");
-    }
-
-    std::vector<const char *> extensionsName(
-        glfwExtensions, glfwExtensions + glfwExtensionCount);
-
-    extensionsName.push_back(vk::KHRPortabilityEnumerationExtensionName);
-    extensionsName.push_back(vk::KHRGetPhysicalDeviceProperties2ExtensionName);
-
-    auto extensionProperties = context.enumerateInstanceExtensionProperties();
-    for (uint32_t i = 0; i < glfwExtensionCount; ++i) {
-      if (std::ranges::none_of(extensionProperties,
-                               [glfwExtension = glfwExtensions[i]](
-                                   auto const &extensionProperty) {
-                                 return strcmp(extensionProperty.extensionName,
-                                               glfwExtension) == 0;
-                               }))
-        throw std::runtime_error("Required GLFW extension not supported: " +
-                                 std::string(glfwExtensions[i]));
-    }
-    vk::InstanceCreateInfo createInfo{
-        .flags = vk::InstanceCreateFlagBits::eEnumeratePortabilityKHR,
-        .pApplicationInfo = &appInfo,
-        .enabledExtensionCount = static_cast<uint32_t>(extensionsName.size()),
-        .ppEnabledExtensionNames = extensionsName.data()};
-
-    instance = vk::raii::Instance(context, createInfo);
-    auto extensions = context.enumerateInstanceExtensionProperties();
-    std::cout << "available extensions:\n";
-
-    for (const auto &extension : extensions) {
-      std::cout << '\t' << extension.extensionName << '\n';
-    }
+  void initVulkan() {
+    createInstance();
+    pickPhysicalDevice();
   }
+
+  void createInstance();
+  bool isDeviceSuitable(vk::raii::PhysicalDevice const &);
+  void pickPhysicalDevice();
 
   void mainLoop() {
     while (!glfwWindowShouldClose(window)) {
